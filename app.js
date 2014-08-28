17e92da8064cb13b9b49775f5bc40a0529104079
garazi <greg@assortedgarbage.com>
2014-08-28 12:41:05 -0700

    Cleaned out old commented out code

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(a127.middleware());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-
 app.all('/:dest', function(req, res, next) {
     if (req.params.dest === "mail") {
         nodemailerConnectorObject.dispatchRequest(req, res);
@@ -29,122 +28,6 @@ app.all('/:dest', function(req, res, next) {
     }
 });
 
-// var cache = cm.create('appCache', {
-//     ttl: 15000
-// });
-
-// app.get('/restaurants', cache.connectMiddleware().cache(), function(req, res, next) {
-//     request('http://localhost:8080/workshop/sandbox/restaurants', function(error, response, body) {
-//         if (error) {
-//             res.send(error);
-//         } else {
-//             res.send(body);
-//         }
-//     });
-// });
-
-// app.get('/restaurants/:id', function(req, res, next) {
-//     async.parallel({
-//             restaurant: function(callback) {
-//                 request("http://localhost:8080/workshop/sandbox/restaurants/?ql=restID=" + req.params.id, function(error, response, body) {
-//                     if (error) {
-//                         res.send(error);
-//                     } else {
-//                         var result = JSON.parse(body);
-//                         callback(null, result);
-//                     }
-//                 });
-//             },
-//             reviews: function(callback) {
-//                 async.waterfall([
-//                     function(callback) {
-//                         request("http://localhost:8080/workshop/sandbox/reviews/?ql=restID=" + req.params.id, function(error, response, body) {
-//                             if (error) {
-//                                 res.send(error);
-//                             } else {
-//                                 data = JSON.parse(body);
-//                                 callback(null, data);
-//                             }
-//                         });
-//                     },
-//                     function(data, callback) {
-//                         var l = data.entities.length;
-//                         var aggregate = 0;
-//                         var i;
-//                         for (i = 0; i < l; i++) {
-//                             aggregate += data.entities[i].rating;
-//                         }
-//                         aggregate = {
-//                             aggregate: +(aggregate / i).toFixed(2)
-//                         }
-//                         callback(null, data, aggregate);
-//                     }
-//                 ], callback);
-//             }
-//         },
-//         function(err, results) {
-//             res.send(results);
-//         });
-// });
-
-// app.get('/reviews', function(req, res) {
-//     request('http://localhost:8080/workshop/sandbox/reviews', function(error, response, body) {
-//         if (error) {
-//             res.send(error);
-//         } else {
-//             res.send(body);
-//         }
-//     });
-// })
-
-// var quota = quotaModule.create({
-//     timeUnit: 'hour',
-//     interval: 1,
-//     allow: 2
-// });
-
-// app.post('/reviews', quota.connectMiddleware().apply({
-//     identifier: 'Foo',
-//     weight: 1
-// }), function(req, res) {
-//     request.post('http://localhost:8080/workshop/sandbox/reviews', {
-//         form: JSON.stringify(req.body)
-//     }, function(error, response, body) {
-//         if(error) {
-//             res.send(error)
-//         } else {
-//             res.send(body);
-//         }      
-//     });
-// });
-/* 
-The following uses the non-middleware implementation of the volos-quota 
-Make sure to comment out the route above if you want to use this one
-*/
-// app.post('/reviews', function(req, res) {
-//     quota.apply({
-//         identifier: 'Foo',
-//         weight: 1
-//     }, function(err, result) {
-//         if (err) {
-//             throw err;
-//         } else {
-//             console.log('Quota status: %s', result.isAllowed);
-//             if (result.isAllowed) {
-//                 request.post('http://localhost:8080/workshop/sandbox/reviews', {
-//                     form: JSON.stringify(req.body)
-//                 }, function(error, response, body) {
-//                     res.send(body);
-//                 });
-//             } else {
-//                 var errorMsg = {
-//                     error: "You have exceeded the hourly quota"
-//                 }
-//                 res.send(JSON.stringify(errorMsg))
-//             }
-//         }
-//     });
-// });
 app.get('/demo-data', routes.demo);
 app.get('/addReview/:id', routes.addreview)
 app.get('/details/:id', routes.details);
@@ -152,10 +35,6 @@ app.get('/', routes.index);
 
 // app.use("/", express.static(__dirname));
 app.use(express.static(__dirname + '/public'));
-
-// app.get('/hello', function(req, res) {
-//     res.send('Hello from Express');
-// });
 
 app.listen('8888', function(req, res) {
     avault.get('garbageVault', function(profileString) {
