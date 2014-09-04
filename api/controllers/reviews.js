3ef00bc8c5514f0dc037bdcf18a4c37491f132b9
garazi <greg@assortedgarbage.com>
2014-09-04 09:13:02 -0700

    demoData zip added

--- api/controllers/reviews.js
+++ api/controllers/reviews.js
@@ -8,18 +8,14 @@ module.exports = {
 }
 
 function getReviews (req, res) {
-	console.log(req.swagger.params)
 	var qs = "";
 	if (req.swagger.params.restID.value) {
 		var restID = req.swagger.params.restID.value;
 		qs = "?ql=restID=" + restID;
-		console.log('restID: %s', restID)
 	} else if (req.swagger.params.rating.value) {
 		var rating = req.swagger.params.rating.value;
-		console.log('rating: %s', rating)
 		qs = "?ql=rating=" + rating;
 	}
-	console.log(qs)
 	request(config.UG + '/reviews' + qs, function(error, response, body) {
         if (error) {
             res.send(error);
