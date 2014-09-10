32e032925819ad7e38167d6d26122019418e5ce4
garazi <greg@assortedgarbage.com>
2014-09-10 09:18:39 -0700

    updated final zip

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
 	// Server address
 	// Server: "http://grewis-test.apigee.net/eats",
-	Server: "http://localhost:8888",
-
+	Server: "http://localhost:10010",
 	// Usergrid path
 	// UG: "http://api.usergrid.com/garazi/sandbox"
 	UG: "http://localhost:8080/workshop/sandbox"
